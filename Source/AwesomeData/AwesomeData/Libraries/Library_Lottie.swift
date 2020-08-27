22f32e9c19cb74976e4e62c833dada3e428c059b
hi.wizman@gmail.com <hi.wizman@gmail.com>
2020-08-27 23:16:40 +0300

    Removed LibraryDemo_Lottie framework

--- Source/AwesomeData/AwesomeData/Libraries/Library_Lottie.swift
+++ Source/AwesomeData/AwesomeData/Libraries/Library_Lottie.swift
@@ -1,5 +1,4 @@
 import Foundation
-import LibraryDemo_Lottie
 
 extension Libraries {
     
--- Source/LibraryDemo_Lottie/LibraryDemo_Lottie/Source/Bundle.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension Bundle {
-    
-    internal static func _current() -> Bundle {
-        return Bundle(identifier: "com.wizman.awesome-libraries.LibraryDemo-Lottie")!
-    }
-}
